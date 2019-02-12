3e64d1b333215f34a15f34eb40e1bf35ca41d69e
Tony Okoth <blairt371.dev@gmail.com>
2019-02-12 12:01:09 +0300

    Fx status and headers response not showing

--- main.js
+++ main.js
@@ -14,21 +14,21 @@ function performGetRequest1(){
  function generateSuccessHTMLOutput(response){
  	return '<h4>Result:</h4>' + 
  	       '<h4>Status:</h4>' +
- 	       '<pre>' + response.status + ' ' + response.statusText + '</pre>',
+ 	       '<pre>' + response.status + ' ' + response.statusText + '</pre>'+
  	       '<h5>Headers:</h5>' + 
- 	       '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>',
+ 	       '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
  	       '<h5>Data</h5>' + 
  	       '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>'
 			 }
 
-			 function generateSuccessHTMLOutput(error){
-				return '<h4>Result:</h4>' + 
-					   '<h5>Message:</h5>' +
-					   '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>',
-					   '<h4>Status:</h4>' +
-					   '<pre>' + error.message + ' ' + error.response.statusText + '</pre>',
-					   '<h5>Headers:</h5>' + 
-					   '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>',
-					   '<h5>Data</h5>' + 
-					   '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'
-						}
+ function generateErrorHTMLOutput(error){
+	return  '<h4>Result:</h4>' + 
+			'<h5>Message:</h5>' +
+			'<pre>' + error.message + '</pre>',
+			'<h5>Status:</h5>' +
+			'<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>',
+			'<h5>Headers:</h5>' + 
+			'<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>',
+			'<h5>Data</h5>' + 
+		    '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'
+			}
