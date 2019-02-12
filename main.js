ea07d2454db106d2f0ff4aee04b05e6b16b8190b
Tony Okoth <blairt371.dev@gmail.com>
2019-02-12 13:19:56 +0300

    Created submit event handler

--- main.js
+++ main.js
@@ -24,11 +24,52 @@ function performGetRequest1(){
  function generateErrorHTMLOutput(error){
 	return  '<h4>Result:</h4>' + 
 			'<h5>Message:</h5>' +
-			'<pre>' + error.message + '</pre>',
+			'<pre>' + error.message + '</pre>' +
 			'<h5>Status:</h5>' +
-			'<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>',
+			'<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>' +
 			'<h5>Headers:</h5>' + 
-			'<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>',
+			'<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' +
 			'<h5>Data</h5>' + 
 		    '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'
 			}
+
+ function performGetRequest2(){
+	 var resultElement = document.getElementById('getResult2');
+	 var todoId = document.getElementById('todoId').value;
+
+ 	resultElement.innerHTML = '';
+
+	 axios.get('http://jsonplaceholder.typicode.com/todos', {
+	    params: {
+			id: todoId
+		}})
+ 	     .then(function(response){
+ 	     	resultElement.innerHTML = generateSuccessHTMLOutput(response);
+ 	     })
+ 	     .catch(function(error){
+ 	     	resultElement.innerHTML = generateErrorHTMLOutput(error);
+		  });
+		}
+
+
+		document.getElementById('todoInputForm').addEventListener('submit', performPostRequest)
+
+		function performPostRequest(e){
+			resultElement = document.getElementById('postResult');
+			var todoTitle = document.getElementById('todoTitle').value;
+	   
+			resultElement.innerHTML = '';
+
+			axios.post('http://jsonplaceholder.typicode.com/todos', {
+				userId: 1,
+				title: todoTitle,
+				completes: false
+			})
+			.then(function(response){
+				resultElement = innerHTML.generateSuccessHTMLOutput(response);
+			})
+			.catch(function(error){
+				resultElement = innerHTML.generateErrorHTMLOutput(error);
+			})
+			e.preventDefault()
+		}
