5c9c8a77af982815b1c4b91adfcfee2b5e6e6d95
Tony Okoth <blairt371.dev@gmail.com>
2019-02-12 11:34:55 +0300

    Response on error

--- main.js
+++ main.js
@@ -19,4 +19,16 @@ function performGetRequest1(){
  	       '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>',
  	       '<h5>Data</h5>' + 
  	       '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>'
- 	        }
+			 }
+
+			 function generateSuccessHTMLOutput(error){
+				return '<h4>Result:</h4>' + 
+					   '<h5>Message:</h5>' +
+					   '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>',
+					   '<h4>Status:</h4>' +
+					   '<pre>' + error.message + ' ' + error.response.statusText + '</pre>',
+					   '<h5>Headers:</h5>' + 
+					   '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>',
+					   '<h5>Data</h5>' + 
+					   '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'
+						}
